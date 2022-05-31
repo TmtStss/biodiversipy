fd587eec8becdd1307c5d28e1aa2cde207d07948
Massimo Ercolani <massimo.ercolani28@gmail.com>
2022-05-31 17:32:11 +0200

    move merge_dfs fn to utils.py

--- biodiversipy/utils.py
+++ biodiversipy/utils.py
@@ -1,15 +1,16 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import rioxarray as rxr
 
-def tif_to_df(file_path, plot=False, coords=False, val='val'):
+def tif_to_df(file_path, plot=False, coords=False, column_name='val'):
     '''
     Function for cleaning one of the 19 worldclim datasets.
     '''
     dataarray = rxr.open_rasterio(file_path)
-    df = dataarray[0].to_pandas()
+    df = dataarray[0].to_pandas().head(100)
 
     if coords:
         # subsetting based on coords dictionary
@@ -44,7 +45,7 @@ def tif_to_df(file_path, plot=False, coords=False, val='val'):
 
     df = df.unstack(level=-1)
     df = df.reset_index()
-    df.columns = ['lon_lower', 'lat_upper', val]
+    df.columns = ['lon_lower', 'lat_upper', column_name]
 
     # Getting upper and lower bounds of boxes
     lon_df = pd.DataFrame({'lon_lower': sorted(df['lon_lower'].unique())})
@@ -57,8 +58,40 @@ def tif_to_df(file_path, plot=False, coords=False, val='val'):
     df = df.merge(lon_df, how='left')
     df = df.merge(lat_df, how='left')
 
-    df = df[['lon_lower', 'lon_upper', 'lat_lower', 'lat_upper', val]]
+    df = df[['lon_lower', 'lon_upper', 'lat_lower', 'lat_upper', column_name]]
 
     df.dropna(inplace=True)
+
+    return df
+
+def merge_dfs(dir_path, coords=False, sort_fn=None, column_name_extractor=lambda file: file):
+    '''
+    Wrapper for get_worldclim_data(). Given a directory, it cleans and merges
+    all datasets in that directory.
+    Description of each bioclimatic variable can be found here: https://worldclim.org/data/bioclim.html
+    '''
+    # get all files in directory
+    files = os.listdir(dir_path)
+    files.sort(key=sort_fn)
+
+    data = {}
+
+    # clean each dataset
+    for file in files:
+        print(file)
+        file_name = os.path.join(dir_path, file)
+        column_name = column_name_extractor(file)
+        df = tif_to_df(file_name, plot=False, coords=coords, column_name=column_name)
+        data[column_name] = df
+
+    # merge datasets
+    i = 0
+    for key in data:
+        if i == 0:
+            df = data[key]
+        else:
+            df = df.merge(data[key], how='inner')
+
+        i += 1
 
     return df
--- biodiversipy/worldclim.py
+++ biodiversipy/worldclim.py
@@ -1,7 +1,7 @@
 import os
 import re
-from utils import tif_to_df
-from params import coords_germany
+from biodiversipy.utils import merge_dfs
+from biodiversipy.params import coords_germany
 
 # Name of directory containing input files
 input_dir = 'wc2.1_30s_bio'
@@ -9,45 +9,17 @@ input_dir = 'wc2.1_30s_bio'
 # Filename of the resulting output
 file_name_out = 'wc2.1_30s_bio_germany.csv'
 
-def merge_worldclim_data(dir_path='../raw_data/wc2.1_30s_bio/', coords=False):
-    '''
-    Wrapper for get_worldclim_data(). Given a directory, it cleans and merges
-    all datasets in that directory.
-    Description of each bioclimatic variable can be found here: https://worldclim.org/data/bioclim.html
-    '''
-    # get all files in directory
-    files = os.listdir(dir_path)
-    files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-
-    data = {}
-
-    # clean each dataset
-    for file in files:
-        print(file)
-        file_name = os.path.join(dir_path, file)
-        val = re.findall('bio_\d+', file)[0]
-        df = tif_to_df(file_name, plot=False, coords=coords, val=val)
-        data[val] = df
-
-    # merge datasets
-    i = 0
-    for key in data:
-        if i == 0:
-            df = data[key]
-        else:
-            df = df.merge(data[key], how='inner')
-
-        i += 1
-
-    return df
-
 if __name__ == '__main__':
     # set absolute paths
     root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path_data = os.path.join(root, 'raw_data', input_dir)
 
     # clean and save data from raw_data folder
-    data = merge_worldclim_data(dir_path=path_data, coords=coords_germany)
+    data = merge_dfs(
+        dir_path=path_data,
+        coords=coords_germany,
+        sort_fn=lambda f: int(''.join(filter(str.isdigit, f))),
+        column_name_extractor=lambda file: re.findall('bio_\d+', file)[0])
 
     path_output = os.path.join(root, 'raw_data')
     data.to_csv(os.path.join(path_output, file_name_out), index=False)

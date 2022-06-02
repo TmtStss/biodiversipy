20b66f1560f6de8b2f9e37f49e992eb1d06649a4
Samuel Drewes <samuel.drewes@gmail.com>
2022-06-02 12:02:39 +0200

    requirements for m1chip

--- biodiversipy/main.py
+++ biodiversipy/main.py
@@ -6,7 +6,8 @@ from biodiversipy.data import get_data
 from biodiversipy.trainer import Trainer
 
 default_params = {
-    'local': True
+    'local': True,
+    'mlflow': True
 }
 
 if __name__ == "__main__":

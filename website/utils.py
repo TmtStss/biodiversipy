500374ed079d67301a7e0c5189b9a469a8fc3dcc
Samuel Drewes <samuel.drewes@gmail.com>
2022-06-03 14:33:35 +0200

    m

--- website/utils.py
+++ website/utils.py
@@ -9,11 +9,8 @@ from icrawler.builtin import GoogleImageCrawler
 import requests
 
 
-<<<<<<< HEAD
 # Gets coordinates from an input location
-=======
 
->>>>>>> 7a038998a66f9e9df146cab4ccaf4240122cd7fd
 def get_coordinates(location):
     url = "https://nominatim.openstreetmap.org/search?"
     params = {"q": location, "format": "json"}
@@ -24,7 +21,6 @@ def get_coordinates(location):
 
     return latitude, longitude
 
-<<<<<<< HEAD
 # 1. Get feature data from lat and long
 
 
@@ -33,8 +29,6 @@ def get_coordinates(location):
 
 
 
-=======
->>>>>>> 7a038998a66f9e9df146cab4ccaf4240122cd7fd
 # Returns species name from a taxonKey and metadata dataframe
 def species_name_from_taxonKey(taxonKey, metadata_df):
 
@@ -50,12 +44,9 @@ def image_from_taxonKey(taxonKey, metadata_df):
     google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'output_image'})
 
     google_Crawler.crawl(keyword = species_name, max_num = 1)
-<<<<<<< HEAD
 
 # Loops through taxonKeys and prints 1 pic for each
 def list_of_species(list_of_taxonKeys, metadata_df):
 
     for taxonKey in list_of_taxonKeys:
         image_from_taxonKey(taxonKey, metadata_df)
-=======
->>>>>>> 7a038998a66f9e9df146cab4ccaf4240122cd7fd

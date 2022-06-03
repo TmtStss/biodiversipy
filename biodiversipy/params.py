d205ea7553c308380ae50347d724914ffe50e6bb
Samuel Drewes <samuel.drewes@gmail.com>
2022-06-03 15:19:41 +0200

    for mikkel

--- biodiversipy/params.py
+++ biodiversipy/params.py
@@ -4,5 +4,13 @@ coords_germany = {
     'lon_upper': 15.4,
     'lat_upper': 55.1}
 
+
+coords_berlin = {
+    'lat_lower': 52.1457991475,
+    'lat_upper': 52.8269473878,
+    'lon_lower': 12.4665964426,
+    'lon_upper': 14.3566136029
+    }
+
 MLFLOW_EXPERIMENT_BASE = "[germany] [berlin] [biodiversipy-team] "
 MLFLOW_URI = "https://mlflow.lewagon.ai/"
